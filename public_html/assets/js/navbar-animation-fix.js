b1c42457762efa84f02568fcc2e838d35b98ae8e
Er1v4ldo <eri.barbosarj@gmail.com>
2017-04-22 22:51:36 -0300

    Correção do bug causado no banner pelo navbar

--- public_html/assets/js/navbar-animation-fix.js
+++ public_html/assets/js/navbar-animation-fix.js
@@ -1,8 +1,7 @@
-$('#collapse-navbar').on('show.bs.collapse', function() {
-	$('.topCasaFina-banner').css('transform', 'translate(-50%, 10%');
-
+$('#menu-nav').on('show.bs.collapse', function (){
+    $('#e-delivery-banner').css('transform', 'translate(-50%, 10%)');
 });
 
-$('#collapse-navbar').on('hide.bs.collapse', function() {
-	$('.topCasaFina-banner').css('transform', 'translate(-50%, -50%');
+$('#menu-nav').on('hide.bs.collapse', function (){
+    $('#e-delivery-banner').css('transform', 'translate(-50%, -50%)');
 });
